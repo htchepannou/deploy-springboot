246ee4155c3af5a91a09f728c0b6b19584f7b54a
htchepannou <htchepannou@expedia.com>
2015-06-17 00:26:29 -0400

    Initial

---
+++ deploy-springboot/install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#
+# Usage: ./install.sh <app-name> <profile>
+#
+
+if [ $# -ne 2 ];then
+    echo "Install failed! Illegal number of parameters"
+    echo "Usage: ./install.sh <app-name> <spring-profile>"
+    exit 1
+fi
+
+APP=$1
+PROFILE=$2
+APP_USER=$1
+APP_GROUP=$1
+
+# Install application
+if [ ! -d "/opt/$APP" ]; then
+  mkdir /opt/$APP
+fi
+
+if [ ! -d "/opt/$APP/log" ]; then
+  mkdir /opt/$APP/log
+fi
+
+cp $APP-1.0-exec.jar /opt/$APP/$APP.jar
+
+chown -R $APP_USER:$APP_GROUP /opt/$APP
+
+
+# /etc/init.d
+cat initd.sh | sed -e "s/__USER_ID__/$APP_USER/" | sed -e "s/__SERVICE_NAME__/$APP/" |  sed -e "s/__ACTIVE_PROFILE__/$PROFILE/" > /etc/init.d/$APP
+
+chmod +x /etc/init.d/$APP
+
+#/etc/init.d/$APP restart
